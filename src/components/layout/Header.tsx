c6cab2fab686da7907063dde24810c0422a97d15
Sivasubramanyam A <ssdragonar5@gmail.com>
2026-06-21 19:27:50 +0530

    Change logo

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -11,7 +11,7 @@ export function Header() {
       <div className="mx-auto flex h-16 max-w-4xl items-center justify-between px-4">
         <Link to="/" className="flex items-end gap-2">
           <img
-            src="https://forum.chennaiastronomyclub.org/uploads/default/original/1X/335c1fe4176468c35ab5f2086d70bc2ff700a7a2.png"
+            src="https://forum.chennaiastronomyclub.org/uploads/default/original/2X/f/f8c566ac2c591ec2a4c89981ca144d0b3770b01a.png"
             alt="Chennai Astronomy Club"
             className="h-8 object-contain"
           />
